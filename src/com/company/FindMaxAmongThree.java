71a54753f8c24e2a70d178576c88388fe5280cd4
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-08 23:52:20 +0530

    Max num among x and y found

--- src/com/company/FindMaxAmongThree.java
+++ src/com/company/FindMaxAmongThree.java
@@ -1,38 +1,46 @@
 package com.company;
 
 public class FindMaxAmongThree<E extends Comparable<E>> {
-    E x;
+    E x, y;
 
-    public FindMaxAmongThree(E x) {
+    public FindMaxAmongThree(E x, E y) {
         this.x = x;
+        this.y = y;
+
     }
 
     public E maximum() {
-        return FindMaxAmongThree.maximum(x);
+        return FindMaxAmongThree.maximum(x, y);
     }
 
-    public static <E extends Comparable<E>> E maximum(E x) {
+    public static <E extends Comparable<E>> E maximum(E x, E y) {
         E max = x;
-        printMax(x, max);
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        printMax(x, y, max);
         return max;
     }
 
-    public static String testMaximum(String x) {
+    public static String testMaximum(String x, String y) {
         String max = x;
-//        printMax(x, max);
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+//        printMax(x, y, max);
         return max;
     }
 
-    public static <E> void printMax(E x, E max) { // E y, E z,
-        System.out.printf("Max num -> " +  x); //y, z,
+    public static <E> void printMax(E x, E y, E max) { // E y, E z,
+        System.out.print("Max num -> " + max); //y, z,
     }
 
     public static void main(String[] args) {
-        Integer xInt = 3;
-        String xStr = " ";
+        Integer xInt = 3, yInt = 4;
+        String xStr = " ", yStr = " ";
 
-        FindMaxAmongThree.testMaximum(xStr);
-        new FindMaxAmongThree(xInt).maximum();
+        FindMaxAmongThree.testMaximum(xStr, yStr);
+        new FindMaxAmongThree(xInt, yInt).maximum();
     }
 
 
