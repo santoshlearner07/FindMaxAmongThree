3e119d6c0a6015aea50a2e40b3857622a1baf150
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-09 00:02:11 +0530

    Max num among x, y and z found

--- src/com/company/FindMaxAmongThree.java
+++ src/com/company/FindMaxAmongThree.java
@@ -1,46 +1,52 @@
 package com.company;
 
 public class FindMaxAmongThree<E extends Comparable<E>> {
-    E x, y;
+    E x, y, z;
 
-    public FindMaxAmongThree(E x, E y) {
+    public FindMaxAmongThree(E x, E y, E z) {
         this.x = x;
         this.y = y;
+        this.z = z;
 
     }
 
     public E maximum() {
-        return FindMaxAmongThree.maximum(x, y);
+        return FindMaxAmongThree.maximum(x, y, z);
     }
 
-    public static <E extends Comparable<E>> E maximum(E x, E y) {
+    public static <E extends Comparable<E>> E maximum(E x, E y, E z) {
         E max = x;
         if (y.compareTo(max) > 0) {
             max = y;
         }
-        printMax(x, y, max);
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        printMax(x, y, z, max);
         return max;
     }
 
-    public static String testMaximum(String x, String y) {
+    public static String testMaximum(String x, String y, String z) {
         String max = x;
         if (y.compareTo(max) > 0) {
             max = y;
         }
-//        printMax(x, y, max);
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
         return max;
     }
 
-    public static <E> void printMax(E x, E y, E max) { // E y, E z,
-        System.out.print("Max num -> " + max); //y, z,
+    public static <E> void printMax(E x, E y, E z, E max) {
+        System.out.print("Max of -> " + x + " " + y + " " + z + " is max -> " + max);
     }
 
     public static void main(String[] args) {
-        Integer xInt = 3, yInt = 4;
-        String xStr = " ", yStr = " ";
+        Integer xInt = 3, yInt = 4, zInt = 5;
+        String xStr = " ", yStr = " ", zStr = " ";
 
-        FindMaxAmongThree.testMaximum(xStr, yStr);
-        new FindMaxAmongThree(xInt, yInt).maximum();
+        FindMaxAmongThree.testMaximum(xStr, yStr, zStr);
+        new FindMaxAmongThree(xInt, yInt, zInt).maximum();
     }
 
 
